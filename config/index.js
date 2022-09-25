077437ad94ebab095be661b9d3bcddf8391745be
yjjssb <114336149+yjjssb@users.noreply.github.com>
2022-09-25 18:48:37 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -63,7 +63,7 @@ const USER_CONFIG = {
   USERS: [oTYba6UxceEVh6l5RtZHtfPuQ2_0]
     {
       // 想要发送的人的名字
-      name: '老婆0',
+      name: '尹大傻子',
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oTYba6UxceEVh6l5RtZHtfPuQ2_0',
       // 你想对他发送的模板消息的模板ID
